ec64958587cf860f5f2ed7093a76f1b8d378a242
Paulo Langer <paulo.langer@xing.com>
2021-03-23 20:48:39 +0000

    implementing the fizzbuzz business logic

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -1,18 +1,30 @@
 defmodule FizzBuzz do
-  @moduledoc """
-  Documentation for `FizzBuzz`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> FizzBuzz.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def build(file_name) do
+    file_name
+    |> File.read()
+    |> handle_file_read
   end
+
+  defp handle_file_read({:ok, content}) do
+    result =
+      content
+      |> String.trim()
+      |> String.split(",")
+      |> Enum.map(&convert_and_evaluate_numbers/1)
+
+    {:ok, result}
+  end
+
+  defp handle_file_read({:error, reason}), do: {:error, "Error reading file: #{reason}"}
+
+  defp convert_and_evaluate_numbers(elem) do
+    elem
+    |> String.to_integer()
+    |> evaluate_number
+  end
+
+  defp evaluate_number(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
+  defp evaluate_number(number) when rem(number, 3) == 0, do: :fizz
+  defp evaluate_number(number) when rem(number, 5) == 0, do: :buzz
+  defp evaluate_number(number), do: number
 end
